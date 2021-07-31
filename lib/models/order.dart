54977c44879c3093ded84aab6673c38e56f2f839
Dip Kapat <kapat.dip@gmail.com>
2021-07-31 13:09:26 +0530

    Added images in assets/images folder, created folders models and data, added files food, order, restaurant & user in models folder and data file in data folder. [DK]

---
+++ lib/models/order.dart
@@ -0,0 +1,17 @@
+
+import 'package:fudlivryz/models/food.dart';
+import 'package:fudlivryz/models/restaurant.dart';
+
+class Order {
+  final Restaurant restaurant;
+  final Food food;
+  final String date;
+  final int quantity;
+
+  Order({
+    required this.date,
+    required this.restaurant,
+    required this.food,
+    required this.quantity,
+  });
+}
